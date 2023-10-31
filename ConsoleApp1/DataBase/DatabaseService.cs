8e1d648242bb5a11268072bd8468d0ef108e6be1
Tymek <lalarsek@gmail.com>
2023-10-31 12:56:13 +0100

    Fixed getAll arguments

--- ConsoleApp1/DataBase/DatabaseService.cs
+++ ConsoleApp1/DataBase/DatabaseService.cs
@@ -127,12 +127,12 @@ namespace Database
             return _databaseContext.Products.FirstOrDefault(p => p.Id == productId);
         }
 
-        public async Task<List<Product>> GetAllOrdersAsync()
+        public async Task<List<Order>> GetAllOrdersAsync()
         {
             return await _databaseContext.Orders.ToListAsync();
         }
 
-        public async Task<List<Product>> GetAllClientsAsync()
+        public async Task<List<Client>> GetAllClientsAsync()
         {
             return await _databaseContext.Clients.ToListAsync();
         }
