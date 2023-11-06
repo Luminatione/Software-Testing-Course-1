93ed02b7b1cf9231e9469e9210df091536ed57ae
Kacprucha <karas.kacper@gmail.com>
2023-11-06 01:49:44 +0100

    Adding UT

---
+++ TestProject1/ClientControllerTest.cs
@@ -0,0 +1,109 @@
+﻿using ConsoleApp1.Controllers;
+using ConsoleApp1.DataBase;
+using ConsoleApp1.Model;
+using Moq;
+
+namespace ConsoleApp1.Controllers.Tests
+{
+    [TestClass]
+    public class ClientControllerTest
+    {
+        [TestMethod]
+        public void CreateClient_AddsClientToDatabase ()
+        {
+            // Arrange
+            int clientId = 1;
+            string name = "John";
+            string secondName = "Doe";
+            string email = "john.doe@example.com";
+
+            var mockDatabaseService = new Mock<IDatabaseService> ();
+            var clientController = new ClientController (mockDatabaseService.Object);
+
+            // Act
+            clientController.CreateClient (clientId, name, secondName, email);
+
+            // Assert
+            mockDatabaseService.Verify (db => db.AddClient (It.IsAny<Client> ()), Times.Once);
+        }
+
+        [TestMethod]
+        public void GetClientById_ReturnsClientFromDatabase ()
+        {
+            // Arrange
+            int clientId = 1;
+            var expectedClient = new Client (clientId, "John", "Doe", "john.doe@example.com");
+
+            var mockDatabaseService = new Mock<IDatabaseService> ();
+            mockDatabaseService.Setup (db => db.GetClientByIdOrNull (clientId)).Returns (expectedClient);
+            var clientController = new ClientController (mockDatabaseService.Object);
+
+            // Act
+            var result = clientController.GetClientById (clientId);
+
+            // Assert
+            Assert.AreEqual (expectedClient, result);
+        }
+
+        [TestMethod]
+        public void GetAllClients_ReturnsListOfClients ()
+        {
+            // Arrange
+            var expectedClients = new List<Client>
+        {
+            new Client(1, "John", "Doe", "john.doe@example.com"),
+            new Client(2, "Jane", "Smith", "jane.smith@example.com"),
+        };
+
+            var mockDatabaseService = new Mock<IDatabaseService> ();
+            mockDatabaseService.Setup (db => db.GetAllClients ()).Returns (expectedClients);
+            var clientController = new ClientController (mockDatabaseService.Object);
+
+            // Act
+            var result = clientController.GetAllClients ();
+
+            // Assert
+            CollectionAssert.AreEqual (expectedClients, result);
+        }
+
+        [TestMethod]
+        public void UpdateClient_UpdatesClientInDatabase ()
+        {
+            // Arrange
+            int clientId = 1;
+            string name = "UpdatedName";
+            string secondName = "UpdatedSecondName";
+            string email = "updated.email@example.com";
+
+            var mockDatabaseService = new Mock<IDatabaseService> ();
+            var existingClient = new Client (clientId, "OriginalName", "OriginalSecondName", "original.email@example.com");
+            mockDatabaseService.Setup (db => db.GetClientByIdOrNull (clientId)).Returns (existingClient);
+            var clientController = new ClientController (mockDatabaseService.Object);
+
+            // Act
+            clientController.UpdateClient (clientId, name, secondName, email);
+
+            // Assert
+            mockDatabaseService.Verify (db => db.UpdateClient (It.IsAny<Client> ()), Times.Once);
+            Assert.AreEqual (name, existingClient.Name);
+            Assert.AreEqual (secondName, existingClient.SecondName);
+            Assert.AreEqual (email, existingClient.Email);
+        }
+
+        [TestMethod]
+        public void DeleteClient_RemovesClientFromDatabase ()
+        {
+            // Arrange
+            int clientId = 1;
+
+            var mockDatabaseService = new Mock<IDatabaseService> ();
+            var clientController = new ClientController (mockDatabaseService.Object);
+
+            // Act
+            clientController.DeleteClient (clientId);
+
+            // Assert
+            mockDatabaseService.Verify (db => db.RemoveClient (clientId), Times.Once);
+        }
+    }
+}
---
+++ TestProject1/ProductControllerTest.cs
@@ -0,0 +1,123 @@
+﻿using ConsoleApp1.Controllers;
+using ConsoleApp1.DataBase;
+using ConsoleApp1.Model;
+using Moq;
+
+namespace ConsoleApp1.Controllers.Tests
+{
+    [TestClass]
+    public class ProductControllerTest
+    {
+        [TestMethod]
+        public void CreateProduct_AddsProductToContext ()
+        {
+            // Arrange
+            int productId = 1;
+            string name = "Product 1";
+            decimal price = 10.0m;
+            int storedAmount = 100;
+
+            var mockContext = new Mock<IDatabaseService> ();
+            var productController = new ProductController (mockContext.Object);
+
+            // Act
+            productController.CreateProduct (productId, name, price, storedAmount);
+
+            // Assert
+            mockContext.Verify (context => context.AddProduct (It.IsAny<Product> ()), Times.Once);
+        }
+
+        [TestMethod]
+        public void GetProductById_ReturnsProductOrNull ()
+        {
+            // Arrange
+            int productId = 1;
+            var expectedProduct = new Product (productId, "Product 1", 10.0m, 100);
+
+            var mockContext = new Mock<IDatabaseService> ();
+            mockContext.Setup (context => context.GetProductByIdOrNull (productId)).Returns (expectedProduct);
+            var productController = new ProductController (mockContext.Object);
+
+            // Act
+            var result = productController.GetProductById (productId);
+
+            // Assert
+            Assert.AreEqual (expectedProduct, result);
+        }
+
+        [TestMethod]
+        public void GetAllProducts_ReturnsAllProducts ()
+        {
+            // Arrange
+            var expectedProducts = new List<Product>
+        {
+            new Product(1, "Product 1", 10.0m, 100),
+            new Product(2, "Product 2", 20.0m, 200)
+        };
+
+            var mockContext = new Mock<IDatabaseService> ();
+            mockContext.Setup (context => context.GetAllProducts ()).Returns (expectedProducts);
+            var productController = new ProductController (mockContext.Object);
+
+            // Act
+            var result = productController.GetAllProducts ();
+
+            // Assert
+            CollectionAssert.AreEqual (expectedProducts, result);
+        }
+
+        [TestMethod]
+        public void UpdateProduct_UpdatesExistingProduct ()
+        {
+            // Arrange
+            int productId = 1;
+            string updatedName = "Updated Product";
+            decimal updatedPrice = 15.0m;
+            int updatedStoredAmount = 150;
+
+            var existingProduct = new Product (productId, "Product 1", 10.0m, 100);
+
+            var mockContext = new Mock<IDatabaseService> ();
+            mockContext.Setup (context => context.GetProductByIdOrNull (productId)).Returns (existingProduct);
+            var productController = new ProductController (mockContext.Object);
+
+            // Act
+            productController.UpdateProduct (productId, updatedName, updatedPrice, updatedStoredAmount);
+
+            // Assert
+            Assert.AreEqual (updatedName, existingProduct.Name);
+            Assert.AreEqual (updatedPrice, existingProduct.Price);
+            Assert.AreEqual (updatedStoredAmount, existingProduct.StoredAmount);
+            mockContext.Verify (context => context.UpdateProduct (existingProduct), Times.Once);
+        }
+
+        [TestMethod]
+        public void UpdateProduct_ThrowsArgumentExceptionForNonexistentProduct ()
+        {
+            // Arrange
+            int productId = 1;
+
+            var mockContext = new Mock<IDatabaseService> ();
+            var productController = new ProductController (mockContext.Object);
+
+            // Act and Assert
+            Assert.ThrowsException<ArgumentException> (() => productController.UpdateProduct (productId, "Updated Product", 15.0m, 150));
+        }
+
+        [TestMethod]
+        public void DeleteProduct_RemovesProductFromContext ()
+        {
+            // Arrange
+            int productId = 1;
+
+            var mockContext = new Mock<IDatabaseService> ();
+            var productController = new ProductController (mockContext.Object);
+
+            // Act
+            productController.DeleteProduct (productId);
+
+            // Assert
+            mockContext.Verify (context => context.RemoveProduct (productId), Times.Once);
+        }
+    }
+}
